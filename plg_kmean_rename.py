c7bd059a36442d0e1e5aad8389379281495c41d3
Estel Freesia <52842527+estelf@users.noreply.github.com>
2022-07-27 00:59:41 +0900

    Add files via upload

---
+++ plg_kmean_rename.py
@@ -0,0 +1,75 @@
+import colorsys
+import cv2
+import numpy as np
+import glob
+import os
+import re
+import cv2
+import numpy as np
+import glob
+import os
+import sys
+import time
+args=sys.argv
+
+
+starts=int(args[3])
+step=int(args[2])
+flname=args[1]
+def my_imread(filename):
+    try:
+        n = np.fromfile(filename, np.uint8)
+        img = cv2.imdecode(n, cv2.IMREAD_COLOR)
+        return img
+    except Exception as e:
+        print(e)
+        return None
+def make_filename(img,basename):
+    name=""
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+    colors_tem= img.reshape(-1, 3)
+    colors=[]
+    for i in colors_tem:
+        if sum(i)>240*3:
+            continue
+        #print(i)
+        colors.append(i)
+    colors= np.array(colors).reshape(-1, 3).astype(np.float32)
+
+    ret,label,center=cv2.kmeans(colors,4,None,criteria,10,cv2.KMEANS_PP_CENTERS)
+    
+    arg_ce=[round(colorsys.rgb_to_hsv(i[2],i[1],i[0])[0]*1000) for i in center]
+    arg_ce.sort(reverse=True)
+    for i in arg_ce:
+        name=str(i)[0:4]+"_"+name
+
+    #cv2.imwrite("color\\"+basename,make_rect(center,50))
+
+    return name+basename
+def main(starts,step,flname):
+    os.chdir(flname)
+    for i,sep in enumerate(glob.glob("*.png")):
+        #print(i,sep)
+        if (i-starts)%step==0:
+            #print(i,starts,step)
+            img=my_imread(sep)
+
+            ####-------------------------------------####
+            try :
+                name=make_filename(img,sep)
+                os.rename(sep,name)
+            except Exception:
+                pass
+            ####-------------------------------------####
+
+
+    os.chdir("..")
+#start_time = time.perf_counter()
+main(starts,step,flname)
+
+
+
+
+
+
