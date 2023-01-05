75ab1411e9e01e731ce516e8e609324304c5342f
Estel_freesia <amenhotp66@gmail.com>
2023-01-05 18:22:11 +0900

    add plg

---
+++ plugins/plg_gray2move.py
@@ -0,0 +1,91 @@
+import glob
+import os
+import re
+import shutil
+import sys
+import time
+
+import cv2
+import numpy as np
+from PIL import Image, ImageStat
+
+args = sys.argv
+
+
+starts = int(args[3])
+step = int(args[2])
+flname = args[1]
+
+
+def my_imread(filename):
+    try:
+        n = np.fromfile(filename, np.uint8)
+        img = cv2.imdecode(n, cv2.IMREAD_COLOR)
+        return img
+    except Exception as e:
+        print(e)
+        return None
+
+
+def my_imwrite(filename, img):
+    try:
+        ext = os.path.splitext(filename)[1]
+        result, n = cv2.imencode(ext, img)
+        if result:
+            with open(filename, mode="w+b") as f:
+                n.tofile(f)
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(e)
+        return False
+
+
+def detect_color_image(file, thumb_size=40, MSE_cutoff=22, adjust_color_bias=True):
+    pil_img = file
+    bands = pil_img.getbands()
+    if bands == ("R", "G", "B") or bands == ("R", "G", "B", "A"):
+        thumb = pil_img.resize((thumb_size, thumb_size))
+        SSE, bias = 0, [0, 0, 0]
+        if adjust_color_bias:
+            bias = ImageStat.Stat(thumb).mean[:3]
+            bias = [b - sum(bias) / 3 for b in bias]
+        for pixel in thumb.getdata():
+            mu = sum(pixel) / 3
+            SSE += sum((pixel[i] - mu - bias[i]) * (pixel[i] - mu - bias[i]) for i in [0, 1, 2])
+        MSE = float(SSE) / (thumb_size * thumb_size)
+        if MSE <= MSE_cutoff:
+            return "g"
+        else:
+            return "c"
+
+
+def main(starts, step, flname):
+    os.chdir(flname)
+    os.makedirs("ggg", exist_ok=True)
+    aldf = glob.glob("*.*")
+    time.sleep(1)
+    for i, sep in enumerate(aldf):
+        if re.search(r".*\.j?pe?n?g$", str(sep), re.I):
+            # print(i,sep)
+            if (i - starts) % step == 0:
+                # print(i,starts,step)
+                img = Image.open(sep)
+
+                # ###-------------------------------------### #
+                if detect_color_image(img) == "g":
+                    shutil.move(sep, "ggg")
+
+                # ###-------------------------------------### #
+
+    os.chdir("..")
+
+
+# start_time = time.perf_counter()
+main(starts, step, flname)
+# end_time = time.perf_counter()
+
+# 経過時間を出力(秒)
+# elapsed_time = end_time - start_time
+# print(elapsed_time,"秒")
